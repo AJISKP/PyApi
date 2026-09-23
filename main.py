b1adfb3852c114a1db0cc4b785422fc7370dce9d
aj <aj@iskpro.com>
2026-09-23 06:24:03 -0500

    initial2

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from queue import PriorityQueue
+
 import requests
 
 api_key ="77eea1d4f27047bd94f93b27fc8c7e8e"
-url="https://newsapi.org/v2/everything?q=tesla&from=2026-08-22&sortBy=publishedAt&apiKey=77eea1d4f27047bd94f93b27fc8c7e8e"
+url="https://newsapi.org/v2/everything?q=tesla&from=2026-08-23&sortBy=publishedAt&apiKey=77eea1d4f27047bd94f93b27fc8c7e8e"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
 request = requests.get(url, headers=headers)
-content = request.text
-print(content)
+content = request.json()
+
+for article in content["articles"]:
+    print(article["title"])
+    print("**********************************")
+    print(article["description"])
+    print(article["url"])
